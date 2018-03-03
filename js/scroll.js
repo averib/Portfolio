3bd1f7504ee6757e943fefa5221c4bdb06aaddf8
Unknown <averibee@gmail.com>
2018-03-03 05:14:41 -0800

    Setup project files and start style guide

---
+++ js/scroll.js
@@ -0,0 +1,17 @@
+
+// testing purposes
+console.log("JavaScript is running on this page");
+
+// ==================================================================
+
+// jquery
+$(document).ready(function() {
+
+    /* 
+    TO ADD LATER:
+    - smooth scrolling
+    - scroll up arrow that appears when user starts scrolling down on page
+        http://jsfiddle.net/robert/fjXSq/
+        https://stackoverflow.com/questions/5980237/show-back-to-top-link-element-using-jquery-when-you-scroll-down
+    */
+});
