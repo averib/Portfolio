99236ab1bf89644403065c2f82d0339e8485df53
Unknown <averibee@gmail.com>
2018-03-03 19:05:58 -0800

    Add smooth scrolling

--- js/scroll.js
+++ js/scroll.js
@@ -4,14 +4,27 @@ console.log("JavaScript is running on this page");
 
 // ==================================================================
 
+/* 
+TO ADD LATER:
+- scroll up arrow that appears when user starts scrolling down on page
+        http://jsfiddle.net/robert/fjXSq/
+        https://stackoverflow.com/questions/5980237/show-back-to-top-link-element-using-jquery-when-you-scroll-down
+*/
+
 // jquery
 $(document).ready(function() {
 
-    /* 
-    TO ADD LATER:
-    - smooth scrolling
-    - scroll up arrow that appears when user starts scrolling down on page
-        http://jsfiddle.net/robert/fjXSq/
-        https://stackoverflow.com/questions/5980237/show-back-to-top-link-element-using-jquery-when-you-scroll-down
-    */
+    // smooth scrolling for all <a> elements to their target
+    $('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash;
+        var $target = $(target);
+
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 1000, 'swing', function () {
+            window.location.hash = target;
+        });
+    });
 });
