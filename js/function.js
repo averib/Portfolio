d5edd3c22f075f2645ef0471d6978fb1f7978125
Unknown <averibee@gmail.com>
2018-03-04 00:40:55 -0800

    Update portfolio
    Started building project process page

--- js/function.js
+++ js/function.js
@@ -1,10 +1,5 @@
 "use strict";
 
-// testing purposes
-console.log("JavaScript is running on this page");
-
-// ==================================================================
-
 /* 
 TO ADD LATER:
 - scroll up arrow that appears when user starts scrolling down on page
@@ -12,9 +7,11 @@ TO ADD LATER:
         https://stackoverflow.com/questions/5980237/show-back-to-top-link-element-using-jquery-when-you-scroll-down
 */
 
-// test
-function openProject() {
-    window.location.href = "ux-ui-project.html"
+// ==================================================================
+
+// open ux/ui project page
+function openProjectUX() {
+    window.location.href = "ux-ui-project.html";
 }
 
 // jquery
