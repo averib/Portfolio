c1e799e91fc4763554f26d04b3b5bf289086f447
Unknown <averibee@gmail.com>
2018-03-29 17:54:35 -0700

    Add scroll down arrow for project pages' overview section
    Art Gallery not included

--- Version 2/js/scroll-view.js
+++ Version 2/js/scroll-view.js
@@ -1,1 +1,24 @@
 "use strict";
+
+// scroll event
+$(window).on("scroll", function() {
+    hideArrow();
+});
+
+// hide down arrow if scrolling out of overview section 
+function hideArrow() {
+    if (isScrolledIntoView(document.getElementsByClassName('shadow-box-small')[0])) {
+        $("#down-arrow").show();
+    } else {
+        $("#down-arrow").hide();
+    }
+}
+
+// checks if an element is completely scrolled into view
+function isScrolledIntoView(el) {
+    var rect = el.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    return isVisible;
+}
