acfd9b9f41909c976529f69d584c41f0f4761868
Unknown <averibee@gmail.com>
2018-04-05 04:07:29 -0700

    Display all final visual design icons and remove src change (on hover)

--- Version 2/js/function.js
+++ Version 2/js/function.js
@@ -16,35 +16,4 @@ $(document).ready(function() {
             window.location.hash = target;
         });
     });
-
-    // hover on final visual design icons
-    $("#icon1").hover(function(){
-       $(this).attr('src', $(this).attr('src').replace('download.png','download-hover.png'));
-    }, function (){
-       $(this).attr('src', $(this).attr('src').replace('download-hover.png','download.png'));
-    })
-
-    $("#icon2").hover(function(){
-       $(this).attr('src', $(this).attr('src').replace('error.png','error-hover.png'));
-    }, function (){
-       $(this).attr('src', $(this).attr('src').replace('error-hover.png','error.png'));
-    })
-
-    $("#icon3").hover(function(){
-       $(this).attr('src', $(this).attr('src').replace('in-queue.png','in-queue-hover.png'));
-    }, function (){
-       $(this).attr('src', $(this).attr('src').replace('in-queue-hover.png','in-queue.png'));
-    })
-
-    $("#icon5").hover(function(){
-       $(this).attr('src', $(this).attr('src').replace('progress-cancel.png','progress-cancel-hover.png'));
-    }, function (){
-       $(this).attr('src', $(this).attr('src').replace('progress-cancel-hover.png','progress-cancel.png'));
-    })
-
-    $("#icon6").hover(function(){
-       $(this).attr('src', $(this).attr('src').replace('finish-download.png','finish-download-hover.png'));
-    }, function (){
-       $(this).attr('src', $(this).attr('src').replace('finish-download-hover.png','finish-download.png'));
-    })
 });
